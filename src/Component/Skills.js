abf1e8d40dec5a06cd45352c6fcd15ba5a64b306
Zaraco <zahrateymouri90@gmail.com>
2020-04-22 16:45:24 +0200

    progressbar added to skills component

--- src/Component/Skills.js
+++ src/Component/Skills.js
@@ -1,19 +1,61 @@
 import React, {Component} from "react";
-import {Carousel, Col, Row} from "react-bootstrap";
+import {Card, Carousel, Col, Container, Jumbotron, ProgressBar, Row} from "react-bootstrap";
 
 class Skills extends Component {
 
     constructor() {
         super();
+        this.state = {
+            skills: [
+                {
+                    name: "React",
+                    percent: 80
+                },
+                {
+                    name: "JavaScript",
+                    percent: 70
+                },
+                {
+                    name: "Npm",
+                    percent: 80
+                }, {
+                    name: "Css",
+                    percent: 80
+                },
+                {
+                    name: "Bootstrap",
+                    percent: 90
+                },
+                {
+                    name: "Photoshop",
+                    percent: 50
+                },
+                {
+                    name: "Sketch",
+                    percent: 70
+                },
+                {
+                    name: "Git",
+                    percent: 80
+                },
+
+            ]
+        }
     }
 
     render() {
+
+        const {skills} = this.state
+        const progressBar = skills.length ? skills.map((skill) =>
+            <ProgressBar style={{marginBottom: '10px'}} animated now={skill.percent} label={`${skill.name} ${skill.percent}%`}/>
+        ) : ''
+
         return (
             <>
                 <Row>
                     <Col>
                         <Carousel>
-                            <Carousel.Item>
+                            <Carousel.Item style={{marginTop: '20px'}}>
                                 <img
                                     className="d-block w-100"
                                     src="Web-Developer-Skills-1.jpg"
@@ -49,10 +91,39 @@ class Skills extends Component {
                 </Row>
 
                 <Row>
-                    <Col>
+                    <Col md={6}>
+                        <Card style={{marginTop: '40px'}}>
+
+                            <Card.Body>
+
+                                <h1>Skills</h1>
+
+                                <Card.Text>These are my skills. You can see in below</Card.Text>
+
+                                <p> {progressBar}</p>
+
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col md={6}>
+
+                        <Jumbotron fluid style={{marginTop: '40px'}}>
+                            <Container>
+                                <h1>What I've been up to.</h1>
+                                <p>
+
+                                </p>
+                            </Container>
+                        </Jumbotron>
 
                     </Col>
+
                 </Row>
+
+
+
+
 
             </>
         );
